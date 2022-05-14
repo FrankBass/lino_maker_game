dda2dd06b4bdd1afcdea413dcd8ba3961be67049
Billy Joe KOM KAMZE <billykamze3@gmail.com>
2022-05-14 14:25:22 +0200

    Ajout de ajax

--- config.php
+++ config.php
@@ -5,10 +5,10 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'lino_makers');
  
-// Connexion � la base de donn�es MySQL 
+// Connexion à la base de données MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// V�rifier la connexion
+// Vérifier la connexion
 if($conn === false){
     die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
 }
